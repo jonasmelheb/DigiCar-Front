53c1b92ea9dc08c7257d8ccb94d30682a4e24fb7
Poyatos Rémi <poyatosremi@gmail.com>
2021-08-27 12:16:09 +0200

    update/ carForCarRental details add some infos about car

--- src/app/common/interfaces/carForCarRentalDetails.model.ts
+++ src/app/common/interfaces/carForCarRentalDetails.model.ts
@@ -1,4 +1,12 @@
+import { ECategory } from "./Ecategory";
+
 export interface CarForCarRentalDetails{
   id: number;
   image: string;
+  mark: string;
+  model: string;
+  placeNumber: number;
+  registration: string;
+  status: string;
+  category: ECategory;
 }
